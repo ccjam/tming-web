22c9e3e66ac723aeb34f1ca7cfc0667d12c421a6
ccjam <swmaestroccjam@gmail.com>
2020-10-08 22:46:28 +0900

    add check update

---
+++ pages/api/checkUpdate.js
@@ -0,0 +1,7 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default (req, res) => {
+    res.statusCode = 200
+    res.json({ fileUrl: 'http://ddddddddd.exe', version: '1.0.4' })
+  }
+  
