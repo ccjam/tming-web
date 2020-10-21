3d2151cd480acd2af367264725ee1f2474731748
yunsuu <ljsql934@gmail.com>
2020-10-21 16:38:01 +0900

    add version code

--- pages/api/checkUpdate.js
+++ pages/api/checkUpdate.js
@@ -2,6 +2,6 @@
 
 export default (req, res) => {
     res.statusCode = 200
-    res.json({ fileUrl: 'https://github.com/ccjam/tming-web/releases/download/1.0.0/Tming.Setup.1.0.9.exe', version: '1.0.9' })
+    res.json({ fileUrl: 'https://github.com/ccjam/tming-web/releases/download/1.0.0/Tming.Setup.1.0.9.exe', version: '1.0.9', versionCode: 1 })
   }
   
